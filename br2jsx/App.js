9dae2b3d2cf2a29363a13ce615670ba33e30fcd1
Anastasiya <marshyna.anastasiya@gmail.com>
2018-10-11 01:49:20 +0300

    replace all <br> in string

---
+++ br2jsx/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BR2JSX from './components/BR2JSX';
+
+let textStr = "This <br> funny tag syntax <br/> is neither a string <br /> nor HTML. It is called JSX, and <br> it is a syntax extension <br/> to JavaScript. We recommend <br /> using it with React to describe what the UI should look like. JSX may remind you of a template language, <br /> but it comes with the full power of <br> JavaScript. JSX produces React “elements”. We will explore rendering them to the DOM in the next section. Below, you can find the basics of JSX necessary to get you started.";
+
+ReactDOM.render(
+  <BR2JSX text = {textStr} />
+  , document.getElementById('container') 
+);
+
---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import './BR2JStyles.css';
+
+class BR2JSX extends React.Component{
+  
+  static propTypes = {
+    //text: PropTypes.string.isRequired
+  };
+  
+  render () {
+   
+    let text = this.props.text;
+    text = text.replace(/<br\s*\/?>/g,  " " ); 
+
+    return text
+  }
+
+}
+
+export default BR2JSX;
+
+
+
