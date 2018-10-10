bc81140c271ab63fe170c713885a2e074dcf132c
Anastasiya <marshyna.anastasiya@gmail.com>
2018-10-10 23:58:27 +0300

    3 borders around this.props.children

---
+++ RainbowFrame/App.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import VotesBlock from './components/VotesBlock';
+
+let questionText='Как вы относитесь к программированию?';
+let answersArr=require('./answers.json');
+let defaultFreeAnswerText="???";
+let colorsArr=['blue','green','orange'];
+
+ReactDOM.render(
+  <VotesBlock 
+    question={questionText}
+    answers={answersArr}
+    deffreeanswertext={defaultFreeAnswerText}
+    startWorkMode={1}
+    colors={colorsArr}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    color: PropTypes.arrayOf(
+      PropTypes.string.isRequired
+    )
+  };
+  
+  render() {
+
+    let childrenBlock = this.props.children;
+    console.log('childrenBlock', childrenBlock);
+    
+    let colorsBorder = this.props.color.map(item => 
+      childrenBlock = (<div key={item} style={{border:"dashed 1px " + item, padding:"10px"}}>
+         {childrenBlock}
+       </div>)
+    );
+    console.log('colorsBorder', colorsBorder);
+    
+    return colorsBorder[colorsBorder.length-1];
+    
+  }
+
+}
+
+export default RainbowFrame;
+
+
+
+
+
