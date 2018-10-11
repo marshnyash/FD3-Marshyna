7cdd0f197af68e9877bbcd65cbb3a23263d9eaf1
Anastasiya <marshyna.anastasiya@gmail.com>
2018-10-11 13:21:34 +0300

    replace all <br> in string

--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -1,18 +1,26 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import './BR2JStyles.css';
+import PropTypes from 'prop-types';
 
 class BR2JSX extends React.Component{
   
   static propTypes = {
-    //text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired
   };
   
   render () {
    
     let text = this.props.text;
-    text = text.replace(/<br\s*\/?>/g,  " " ); 
+    //text = text.replace(/<br\s*\/?>/g,  '\r\n' ); // так только в консоле показывает верно ()
+    text = text.split(/<br\s*\/?>/g); 
+    console.log(text);
 
+    text = text.map(item => {
+        return <div>{item}</div>
+    });
+    
+    console.log(text);
     return text
   }
 
